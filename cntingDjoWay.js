a3e45be9f974ba7fc631df87f5a66f6ca21aab56
smilliononline <million_18@outlook.com>
2018-05-05 15:06:33 -0500

    commit

---
+++ cntingDjoWay.js
@@ -0,0 +1,11 @@
+for (var i = 1; i <= 100; i++){
+    if (i % 5 === 0){
+        console.log(i, "Coding");
+    }
+    if (i % 10 === 0){
+        console.log(i, "Dojo");
+    }
+    else{
+        console.log(i);
+    }
+}
