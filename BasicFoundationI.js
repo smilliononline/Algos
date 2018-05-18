6cd61e597f0c2947583b438c793d2a951017b430
smilliononline <million_18@outlook.com>
2018-05-17 22:45:48 -0500

    Basic Foundation I

---
+++ BasicFoundationI.js
@@ -0,0 +1,53 @@
+/*GEt 1 to 255:
+
+function a(){
+    var arr = [];
+    for(i=0; i< 256; i++){
+        arr.push(i);
+       
+    }
+    return arr;
+}
+console.log(a()); */
+
+
+/*Get even 1,000
+
+function even(){
+    var sum = 0;
+    for(i = 0; i < 1001; i+=2){
+        sum = sum + i;
+    }
+    return sum;
+}
+console.log(even());
+answer = 250,200 */
+
+
+/* Sum odd 5,000
+
+function odd (){
+    var sum = 0;
+    for(i = 1; i <5001; i+=2){
+        sum = sum + i;
+    }
+    return sum;
+}
+console.log(odd());
+answer = 6250000 */
+
+
+/*Iterate an array
+
+function total(arr){
+  sum = 0;
+  for(i=0; i<arr.length; i++){
+    sum = sum + arr[i]
+  }
+  return sum;
+}
+console.log(total([2,2,2]));
+answer = 6 */
+
+
+
---
+++ whtDoUKnw.js
@@ -0,0 +1,6 @@
+for (var i = -300; i <= 300; i++ ){
+    if(i % 2 != 0){
+     sum = i + i++;
+        console.log(sum);
+    }
+}
