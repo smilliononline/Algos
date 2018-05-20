5416a4e80bfab706aa5d7ab81bee9fced2064af5
smilliononline <million_18@outlook.com>
2018-05-19 23:19:02 -0500

    update

---
+++ IntermediateFoundation.js
@@ -0,0 +1,7 @@
+function sigma(num){
+    for(i=1; i <= num; i+i){
+        console.log(i);
+    }
+}
+
+console.log(sigma(10));
---
+++ suckerHuge.js
@@ -0,0 +1,9 @@
+sum = 0;
+for(i = -300000; i <300001; i++){
+    if(i % 2 != 0){
+        sum = sum + i;
+    }
+    console.log(sum);
+}
+
+answer = -2575265975
